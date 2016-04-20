5f451fb391972cab1f63ad372219f9e827321334
Felza <pet_nysen@yaho.dk>
2016-04-20 17:04:24 +0200

    more cords

--- Drone_group_11/src/coordinateSystem/Coordinates.java
+++ Drone_group_11/src/coordinateSystem/Coordinates.java
@@ -2,8 +2,11 @@ package coordinateSystem;
 
 import java.util.ArrayList;
 
+import helper.Point;
+
 public class Coordinates {
 	ArrayList<Cord> cordList;
+	double picScale = 0.1; //!!!!! skal scaleres så picture passer til valgt frame størrelse i rummet
 	
 	public ArrayList<Cord> getCords() {
 		return cordList;
@@ -17,6 +20,15 @@ public class Coordinates {
 		cordList.add(new Cord(x,y,green));
 	}
 	
-
+	public void addCords(ArrayList<Cord> tempList, double altitude, Point currentPos){
+		// get altitude
+		double altScale = altitude * 0.23; //0.23 skal rettes til whatever
+		
+		
+		for(Cord cord : tempList){
+			addCord(cord.x*altScale+currentPos.getX(),cord.y*altScale+currentPos.getY(),cord.green);
+		}
+	}
+	
 }
 
