042e4e51c13027c3a0004b624cea232857170365
Toby <tobiasmohr22@gmail.com>
2016-06-09 09:35:37 +0200

    s

--- Drone_group_11/src/picture/PictureController.java
+++ Drone_group_11/src/picture/PictureController.java
@@ -220,7 +220,7 @@ public class PictureController  {
 	public void grabFromVideo() throws org.bytedeco.javacv.FrameGrabber.Exception {
 		OpenCVFrameConverter.ToIplImage converter = new OpenCVFrameConverter.ToIplImage();
 		OpenCVFrameConverter.ToMat converterMat = new OpenCVFrameConverter.ToMat();
-		FrameGrabber grabber = new VideoInputFrameGrabber(1);
+		FrameGrabber grabber = new VideoInputFrameGrabber(0);
 		grabber.start();
 
 		Runnable frameGrabber = new Runnable() {
--- Drone_group_11/src/picture/PictureProcessingHelper.java
+++ Drone_group_11/src/picture/PictureProcessingHelper.java
@@ -212,7 +212,7 @@ public class PictureProcessingHelper {
 		double sum = (w*y)/(2*x*z);
 		double AOV = Math.atan(sum)*2;
 		AOV = Math.toDegrees(AOV);
-//		System.out.println("AOV " + AOV);
+		System.out.println("AOV " + AOV);
 		
 		Point tl = null;
 		Point tr = null;
@@ -317,12 +317,12 @@ public class PictureProcessingHelper {
 			br = new Point((int) vertices.position(0).x(), (int) vertices.position(0).y());
 			bl = new Point((int) vertices.position(1).x(), (int) vertices.position(1).y());
 		}
-		System.out.println("----------");
-		System.out.println(Math.toDegrees(calculateAngle(tl, tr, bl)));
-		System.out.println(Math.toDegrees(calculateAngle(tr, tl, br)));
-		System.out.println(Math.toDegrees(calculateAngle(bl, tl, br)));
-		System.out.println(Math.toDegrees(calculateAngle(br, bl, tr)));
-		System.out.println("----------");
+//		System.out.println("----------");
+//		System.out.println(Math.toDegrees(calculateAngle(tl, tr, bl)));
+//		System.out.println(Math.toDegrees(calculateAngle(tr, tl, br)));
+//		System.out.println(Math.toDegrees(calculateAngle(bl, tl, br)));
+//		System.out.println(Math.toDegrees(calculateAngle(br, bl, tr)));
+//		System.out.println("----------");
 	}
 	
 	public boolean scanQrCode(Mat srcImage){
