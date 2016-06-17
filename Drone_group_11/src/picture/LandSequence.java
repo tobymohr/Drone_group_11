f1f1edc9a482fd63d18741197e54495997512091
Mkiza <Mkiza@10.16.169.178>
2016-06-17 10:29:00 +0200

    One last tweak and we party for a week

--- Drone_group_11/src/picture/LandSequence.java
+++ Drone_group_11/src/picture/LandSequence.java
@@ -3,9 +3,11 @@ package picture;
 import static org.bytedeco.javacpp.opencv_imgproc.minAreaRect;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.bytedeco.javacpp.opencv_core.Mat;
+import org.bytedeco.javacpp.opencv_core.RotatedRect;
 
 import app.CommandController;
 import helper.Command;
@@ -38,16 +40,6 @@ public class LandSequence implements Runnable {
 		this.camMat = camMat;
 	}
 
-
-	public enum VideoBitRateMode{
-		DYNAMIC
-	}
-
-	public enum VideoCodec{
-		H264_720P_SLRS
-	}
-
-
 	public void run() {
 
 		System.out.println("HOVER");
@@ -55,112 +47,101 @@ public class LandSequence implements Runnable {
 		while (code == null) {
 			code = OFC.scanQrCode(camMat);
 			sleep(10);
-			if(code != null){
-				System.out.println(code);
-				break;
-			}
-
 		}
 		System.out.println(code);
 
-		sleep(5000);
+		sleep(1900);
 		cC.dC.setSpeed(5);
-		cC.addCommand(Command.UP, 3600, 40);
-		sleep(3700);
+		cC.addCommand(Command.UP, 2600, 15);
+		sleep(2600);
 		cC.dC.hover();
 
 		while (true) {
 			circles = OFC.myCircle(camMat);
 			if (circles > 0) {
-				cC.addCommand(Command.DOWN, 2000, 15);
+				cC.addCommand(Command.DOWN, 1000, 20);
 				sleep(2000);
-				while (checkCode == null) {
-					checkCode = OFC.scanQrCode(camMat);
-					sleep(10);
-				}
-				cC.addCommand(Command.UP, 2500, 20);
+//				while (checkCode == null) {
+//					checkCode = OFC.scanQrCode(camMat);
+//					sleep(10);
+//				}
+				cC.addCommand(Command.UP, 1000, 20);
 				sleep(2100);
 				if (code.equals(checkCode)) {
 					System.out.println("Found");
 					// TODO: Save coordinates
 					break;
 				}
-
+				
 			}
 		}
+
 		while (true) {
-
-			boolean check = OFC.checkDecodedQR(camMat);
-
-			if (check) {
-				System.out.println("checked");
-				circles = OFC.myCircle(camMat);
-				cC.addCommand(Command.DOWN, 1000, 30);
-				sleep(1200);
-				if (circles > 0) {
-					aboveLanding = true;
-					// If false restart landing sequence
-					// Land
-					System.out.println("going down");
-					cC.addCommand(Command.DOWN, 500, 50);
-					// Thread.sleep(10);
-					sleep(1000);
-					counts++;
-					// System.out.println(counts);
-					circleCounter = 0;
-				} else {
-					circles = 0;
-					circleCounter++;
-					System.out.println(circleCounter);
-				}
-				if (circleCounter >= 120) {
-					aboveLanding = false;
-					circleCounter = 0;
-					counts = 0;
-				}
-				if (counts >= 3) {
-					System.out.println("landing");
-					cC.dC.emergencyStop();
-				}//					cC.addCommand(Command.LAND, 6000, 2);
-
-					if (circles > 0 || check) {
-						System.out.println("emergency stop");
-						cC.dC.emergencyStop();
-						break;
-					}
-				}
-			}
-			
-			// if (circles > 0) {
-			//
-			// aboveLanding = true;
-			// System.out.println("going down");
-			//
-			// sleep(10);
-			// counts++;
-			// circleCounter = 0;
-			// } else {
-			// circles = 0;
-			// circleCounter++;
-			// System.out.println(circleCounter);
-			//
-			// }
-			// if (circleCounter >= 120) {
-			// aboveLanding = false;
-			// circleCounter = 0;
-			// counts = 0;
-			// }
-			// if (counts >= 3) {
-			// System.out.println("landing");
-			//
-			// cC.addCommand(Command.LAND, 6000, 2);
-			// break;
-			// }
+		
+		circles = OFC.myCircle(camMat);
+		if (circles > 0) {
+			cC.addCommand(Command.DOWN, 1000, 20);
+			sleep(2000);
 		}
-
+		
+		boolean check = OFC.checkDecodedQR(camMat);
+		
+		if(check || circles > 0){
+			cC.dC.land();
+		}
+		
+		
+		}
+//
+//			boolean check = OFC.checkDecodedQR(camMat);
+//
+//			if (check) {
+//				System.out.println("checked");
+//				circles = OFC.myCircle(camMat);
+//				cC.addCommand(Command.DOWN, 1000, 20);
+//				sleep(1200);
+//				if (circles > 0) {
+//					while (true) {
+//						System.out.println("found circle");
+//
+//						
+//						if (circles > 0 || check) {
+//							cC.dC.land();
+//							break;
+//						}
+//
+//					}
+//				}
+//				// if (circles > 0) {
+//				//
+//				// aboveLanding = true;
+//				// System.out.println("going down");
+//				//
+//				// sleep(10);
+//				// counts++;
+//				// circleCounter = 0;
+//				// } else {
+//				// circles = 0;
+//				// circleCounter++;
+//				// System.out.println(circleCounter);
+//				//
+//				// }
+//				// if (circleCounter >= 120) {
+//				// aboveLanding = false;
+//				// circleCounter = 0;
+//				// counts = 0;
+//				// }
+//				// if (counts >= 3) {
+//				// System.out.println("landing");
+//				//
+//				// cC.addCommand(Command.LAND, 6000, 2);
+//				// break;
+//				// }
+//			}
+//		}
+	}
 
 	// }
-
 
 	private void sleep(int duration) {
 		try {
