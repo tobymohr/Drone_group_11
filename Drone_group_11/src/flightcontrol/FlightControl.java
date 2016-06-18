ea0a87e35596179cfeb32fd7b01eefe527b02609
Kåre Jørgensen <kaare1994@gmail.com>
2016-06-19 01:03:58 +0200

    check aspect ratio

--- Drone_group_11/src/flightcontrol/FlightControl.java
+++ Drone_group_11/src/flightcontrol/FlightControl.java
@@ -19,6 +19,8 @@ import picture.PictureController;
 import picture.PictureProcessingHelper;
 
 public class FlightControl implements Runnable {
+	public static final double CENTER_UPPER = 0.1;
+	public static final double CENTER_LOWER = -0.1;
 
 	private PictureProcessingHelper pictureProcessingHelper = new PictureProcessingHelper();
 	private CommandController commandController;
@@ -63,127 +65,122 @@ public class FlightControl implements Runnable {
 
 	public void flyLaneOne() {
 		commandController.droneInterface.setFrontCamera();
-		//		commandController.addCommand(Command.ROTATELEFT, 4000, 30); // spin 180
-		//		sleepThread(4500);
+		// commandController.addCommand(Command.ROTATELEFT, 4000, 30); // spin
+		// 180
+		// sleepThread(4500);
 
 		List<Mat> contours = pictureProcessingHelper.findQrContours(camMat);
-		RotatedRect rect = null;
+		RotatedRect rect = rightMostRect(contours);
 		Mat qrImg = null;
 		String tempCode = "";
 		int i = 0;
-		// TODO: noget aspect ratio, så vi ved vi kigger på en af de rect på væg over for dronen
 		double distance = pictureProcessingHelper.calcDistance(rect);
 		double currentDistance = pictureProcessingHelper.calcDistance(rect);
 		adjustToRectDistance(rect);
 		do {
-			
+			commandController.addCommand(Command.FORWARD, 1500, 15);
 			sleepThread(2500);
-			//hover
+			// hover
 			commandController.droneInterface.setBottomCamera();
-			do{
+			do {
 				downScan.scanGreen();
 				downScan.scanRed();
-			}while(!downScan.greenDone && !downScan.redDone);
+			} while (!downScan.greenDone && !downScan.redDone);
 			downScan.greenDone = false;
 			downScan.redDone = false;
 
-			
 			// TODO: Scan for immediate threats
 			// boxes
-			
-			// adjust position relative to mostRightRect with correct aspect ratio
+
 			contours = pictureProcessingHelper.findQrContours(camMat);
 			if (contours.size() > 0) {
-				rect = mostCenteredRect(contours);
+				rect = rightMostRect(contours);
 				qrImg = pictureProcessingHelper.warpImage(camMat, rect);
 				tempCode = pictureProcessingHelper.scanQrCode(qrImg);
 			}
-		} while (!tempCode.startsWith("W00.04"));
+		} while (!tempCode.startsWith("W00"));
 
-		
-//		
-//		System.out.println("FOUND");
-//		contours = pictureProcessingHelper.findQrContours(camMat);
-//		rect = rightMostRect(contours);
-//		
-		
+		//
+		// System.out.println("FOUND");
+		// contours = pictureProcessingHelper.findQrContours(camMat);
+		// rect = rightMostRect(contours);
+		//
+
 		// Vi kan nu se QR kode og centrerer på den
 		do {
-			// kåre centrere QR kode 
+			// kåre centrere QR kode
 			// check distance to QR kode
-//			contours = pictureProcessingHelper.findQrContours(camMat);
-//			rect = mostCenteredRect(contours);
-//			distance = pictureProcessingHelper.calcDistance(rect);
-			
-			
+			// contours = pictureProcessingHelper.findQrContours(camMat);
+			// rect = mostCenteredRect(contours);
+			// distance = pictureProcessingHelper.calcDistance(rect);
+
 			commandController.addCommand(Command.FORWARD, 1000, 10);
 			sleepThread(1500);
-			
-			commandController.droneInterface.setBottomCamera();
-		do{
-			downScan.scanGreen();
-			downScan.scanRed();
-		}while(!downScan.greenDone && !downScan.redDone);
-		downScan.greenDone = false;
-		downScan.redDone = false;
 
-			
+			commandController.droneInterface.setBottomCamera();
+			do {
+				downScan.scanGreen();
+				downScan.scanRed();
+			} while (!downScan.greenDone && !downScan.redDone);
+			downScan.greenDone = false;
+			downScan.redDone = false;
+
 		} while (distance > 100);
 		System.out.println("CLOSE");
 	}
+	
+	private boolean checkAspectRatio(RotatedRect rect) {
+		double center = pictureProcessingHelper.center(rect);
+		if (center > CENTER_UPPER || center < CENTER_LOWER) {
+			return false;
+		}
+		return true;
+	}
 
-
-
-
-
-	private RotatedRect rightMostRect(List<Mat> contours){
+	private RotatedRect rightMostRect(List<Mat> contours) {
 		double distanceFomCenter = Double.MAX_VALUE;
 		RotatedRect rect = new RotatedRect();
 		for (int i = 0; i < contours.size(); i++) {
 			RotatedRect rect2 = minAreaRect(contours.get(i));
 			double distance = (camMat.arrayWidth() / 2) - rect.center().x();
-			if (distanceFomCenter > distance) {
+			if (distanceFomCenter > distance && checkAspectRatio(rect2)) {
 				distanceFomCenter = distance;
 				rect = rect2;
 			}
 		}
 		return rect;
 	}
+	
 
-	private RotatedRect mostCenteredRect(List<Mat> contours){
+	private RotatedRect mostCenteredRect(List<Mat> contours) {
 		double distanceFomCenter = Double.MAX_VALUE;
 		RotatedRect rect = new RotatedRect();
 		for (int i = 0; i < contours.size(); i++) {
 			RotatedRect rect2 = minAreaRect(contours.get(i));
 			double distance = (camMat.arrayWidth() / 2) - rect.center().x();
-			if (distanceFomCenter > distance) {
+			if (distanceFomCenter > distance && checkAspectRatio(rect2)) {
 				distanceFomCenter = Math.abs(distance);
 				rect = rect2;
 			}
 		}
 		return rect;
 	}
-	
-	private void adjustToRectDistance(RotatedRect rect){
+
+	private void adjustToRectDistance(RotatedRect rect) {
 		distance = pictureProcessingHelper.calcDistance(rect);
 		commandController.addCommand(Command.FORWARD, 1000, 10); // 1 chunk
-		while(!tooClose && !tooFar){
+		while (!tooClose && !tooFar) {
 			currentDistance = pictureProcessingHelper.calcDistance(rect);
-			if((distance - currentDistance) > 95){
+			if ((distance - currentDistance) > 95) {
 				commandController.addCommand(Command.BACKWARDS, 500, 10);
 			}
-			if((distance - currentDistance) < 75){
+			if ((distance - currentDistance) < 75) {
 				commandController.addCommand(Command.FORWARD, 500, 10);
 			}
 		}
 	}
 
-
-
-
-
-	private void sleepThread(int duration)
-	{
+	private void sleepThread(int duration) {
 		try {
 			Thread.sleep(duration);
 		} catch (InterruptedException e) {
@@ -191,19 +188,21 @@ public class FlightControl implements Runnable {
 		}
 	}
 
-
 	private void flyLaneTwo() {
 		// TODO Auto-generated method stub
 
 	}
+
 	private void flyLaneThree() {
 		// TODO Auto-generated method stub
 
 	}
+
 	private void flyLaneFour() {
 		// TODO Auto-generated method stub
 
-	}	
+	}
+
 	private void flyLaneFive() {
 		// TODO Auto-generated method stub
 
